6a46e2d64d1ef46829096397a9c17defc2a6e366
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-17 17:09:35 +0100

    add validations for username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
     
     has_many :chocolates #that they have created
     
+    validates :username, uniqueness: true,  presence: true
+    validates :email, presence: true
+
     has_secure_password #adds authomaticly authenticate method and says validation for password is true and also validate password coformation if there is one
 end
