b1e118c932e940b86f25168e7dc030e7e7e5c903
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-17 17:01:17 +0100

    add create action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,15 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-
+    def create
+        @user = User.new(user_params)
+        if @user.save #if I can save user which means user is valid then we sign up user and redirect to his page show
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          render :new
+        end
+    end
     
     def show
         @user = User.find_by_id(params[:id])
