c4846dad4f1a918215c8a91837b5fc7ee7e1e91b
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-18 00:14:21 +0100

    add chocolate_params

--- app/controllers/chocolates_controller.rb
+++ app/controllers/chocolates_controller.rb
@@ -1,5 +1,13 @@
 class ChocolatesController < ApplicationController
     def new
         @chocolate = Chocolate.new
+        @chocolate.build_brand #becouse of nested form in new
     end
+
+    
+  private
+
+  def chocolate_params
+    params.require(:chocolate).permit(:category, :description, :brand_id, brand_attributes: [:name])
+  end
 end
