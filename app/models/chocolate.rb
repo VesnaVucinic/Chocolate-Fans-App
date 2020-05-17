5d65e58a647899fdf35cb27956083f960c6c4279
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-17 16:01:21 +0100

    add relationships to models

--- app/models/chocolate.rb
+++ app/models/chocolate.rb
@@ -1,4 +1,6 @@
 class Chocolate < ApplicationRecord
   belongs_to :brand
-  belongs_to :user
+  belongs_to :user #creator of it, gives all singular instances
+  has_many :reviews
+  has_many :users, through: :reviews #people wh have reviewed it, gives all plural instances
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :reviews
+  has_many :reviewed_chocolates, through: :reviews, source: :chocolate #no plural, exectly match like in review table
+  
+  has_many :chocolates #that they have created
+
 end
