6a352f05189f0314af5bc3c58cc2b9e7bb5079b8
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-18 00:19:10 +0100

    add create action in chocolate_controller

--- app/controllers/chocolates_controller.rb
+++ app/controllers/chocolates_controller.rb
@@ -4,6 +4,15 @@ class ChocolatesController < ApplicationController
         @chocolate.build_brand #becouse of nested form in new
     end
 
+
+    def create
+        @chocolate = Chocolate.new(chocolate_params)
+        if @chocolate.save #this is where validations happen
+            redirect_to chocolate_path(@chocolate)
+        else
+            render :new
+        end
+     end
     
   private
 
