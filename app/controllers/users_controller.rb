12453bf595c1b65ea31c223eecf8ae4c12a661c1
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-17 16:48:24 +0100

    add user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,15 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+
     
-  def show
-    @user = User.find_by_id(params[:id])
-  end
+    def show
+        @user = User.find_by_id(params[:id])
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
