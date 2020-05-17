ae6264087f2f38a53d83be73056f6a026d1e6925
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-17 20:34:31 +0100

    add log in action create and desplay errors in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,4 +10,18 @@ class SessionsController < ApplicationController
     
     def new
     end
+
+    def create
+        #Try to find the user in the system
+        @user = User.find_by(username: params[:user][:username]) #find can only with id, find_by better becouse don't throw error
+        
+        if @user.try(:authenticate, params[:user][:password])#if there is user and authenticate we set up session and log in
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Sorry, login info was incorrect. Please try again."
+            redirect_to login_path #error page we will rederct becouse it's harder is to guess, harder if your user name isn't persisted when they guess wrong
+        end
+    end
+  
 end
