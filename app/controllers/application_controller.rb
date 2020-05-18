eaf621490b1bac5d0a36ef5a2c835f8c8931a2a1
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-18 14:02:02 +0100

    add current-user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+    private
+  
+    def current_user
+      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id] #will always call db once if I use current_user miore that once
+    end
 end
