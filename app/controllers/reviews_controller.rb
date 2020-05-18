1734d2bfbc318f539c0f569864d366c362fd5408
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-18 13:04:21 +0100

    add new action in controller and new form in view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+
+    def new
+        @chocolate = Chocolate.find_by_id(params[:chocolate_id])# once we have nesed rout it's not :id it's chocolate_id
+        @review = @chocolate.reviews.build #review must know chocolate it's associated with, instatead taht already know about chocolate 
+    end
+
+    def index
+    end
+
 end
