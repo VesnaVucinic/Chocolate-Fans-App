051c2c77834c3a065b69c5d46512aa7d16df9539
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-18 13:50:42 +0100

    add validations for title and star

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :chocolate
   belongs_to :user
+
+  validates :title, presence: true
+  validates :stars, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than: 6}
+
+
+  
 end
