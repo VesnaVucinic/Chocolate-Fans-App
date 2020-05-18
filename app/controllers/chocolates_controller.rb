b67ccd1ab45285259230a9fdb9066a61ab9b6353
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-18 17:47:06 +0100

    add correction

--- app/controllers/chocolates_controller.rb
+++ app/controllers/chocolates_controller.rb
@@ -12,7 +12,7 @@ class ChocolatesController < ApplicationController
 
     def create
         @chocolate = Chocolate.new(chocolate_params)
-        @ice_cream.user_id = session[:user_id] # becouse we have user.id in chocolate table
+        @chocolate.user_id = session[:user_id] # becouse we have user.id in chocolate table
         if @chocolate.save #this is where validations happen
             redirect_to chocolate_path(@chocolate)
         else
