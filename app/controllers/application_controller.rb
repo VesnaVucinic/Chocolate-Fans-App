3c218617920fffcbaa5ba7ba0aef17324adcbbd0
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-18 18:36:50 +0100

    add syntax error correction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     private
   
     def current_user
-      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id] #will always call db once if I use current_user miore that once
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
 end
