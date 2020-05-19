f747a6d927f710d0fd648bc6dba2b8c5d1a6ca0c
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-19 21:39:40 +0100

    add display_header method

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,10 @@
 module ReviewsHelper
+
+    def display_header(review)
+      if params[:chocolate_id]
+          content_tag(:h1, "Add a Review for #{review.chocolate.title} -  #{review.chocolate.brand.name}")
+      else
+        content_tag(:h1, "Create a review")
+      end
+    end
 end
