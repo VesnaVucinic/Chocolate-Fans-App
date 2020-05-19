24a8ed85e6feccfb1f00be2aa1d50012870cc35c
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-19 17:01:52 +0100

    add custom message for reviewed chocolate

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,6 +6,7 @@ class Review < ApplicationRecord
   validates :title, presence: true
   validates :stars, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than: 6}
 
+  validates :chocolate, uniqueness: { scope: :user, message: "has already been reviewed by you"  }
 
   
 end
