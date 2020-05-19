ba11cc86bc87fa55d9c07e100475c9ec87e280d3
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-19 22:59:12 +0100

    add link, new action and form for brand

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -4,5 +4,21 @@ class BrandsController < ApplicationController
   def index
     @brands = Brand.all
   end
+
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.create(brand_params)
+    redirect_to brands_path(@brands)
+  end
+
+  private
+
+  def brand_params
+    params.require(:brand).permit(:name)
+  end
+
     
 end
