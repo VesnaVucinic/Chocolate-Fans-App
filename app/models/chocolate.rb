4bee8354458e60640bc97aeb18c79270ea785659
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-19 14:52:06 +0100

    add not_a_duplicate method

--- app/models/chocolate.rb
+++ app/models/chocolate.rb
@@ -4,13 +4,23 @@ class Chocolate < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews #people wh have reviewed it, gives all plural instances
 
-  #accepts_nested_attributes_for :brand
+  accepts_nested_attributes_for :brand
 
-  validates :name, :category, :description, presence: true
-  
+  validates :title, :category, :description, presence: true
+  #validate :not_a_duplicate
+
   
   def brand_attributes=(attributes)
     self.brand = Brand.find_or_create_by(attributes) if !attributes['name'].empty?
     self.brand
+  end
+
+
+  def not_a_duplicate
+    # if there is already an chocolate with that name && brand, throw an error
+    chocolate = Chocolate.find_by(title: title, brand_id: brand_id) # name is just like calling self.name - instance.name
+    if !!chocolate && chocolate != self
+      errors.add(:title, 'has already been added for that brand')
+    end
   end
 end
