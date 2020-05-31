396beb08540cbd1d3efeea810c9c8f92abd05854
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-05-31 11:20:50 +0100

    add validates rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,11 +4,11 @@ class Review < ApplicationRecord
  
 
   validates :title, presence: true
-  validates :stars, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than: 6}
+  validates :rating, numericality: true 
 
   validates :chocolate, uniqueness: { scope: :user, message: "has already been reviewed by you"  }
-   #custom message, validate og 2 things icluded, I only want users to make one review for chocolate,
-   # can't review twice same chocolate, gives nicer erroe message
+   #validate og 2 things icluded, I only want users to make one review for chocolate,
+   # can't review twice same chocolate, gives nicer erroe message custom message, 
 
-  
+
 end
