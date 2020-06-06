29e75abd9cc1049377232ddae24e70b0cd7e7e6c
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-06-06 14:46:19 +0100

    add blank_stars method

--- app/models/review.rb
+++ app/models/review.rb
@@ -10,5 +10,7 @@ class Review < ApplicationRecord
    #validate og 2 things icluded, I only want users to make one review for chocolate,
    # can't review twice same chocolate, gives nicer erroe message custom message, 
 
-
+   def blank_stars
+    5 - rating.to_i
+   end
 end
